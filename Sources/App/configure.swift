4a7d9de062732d0594c9518b327d4c0113dc41ce
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-03 14:27:20 -0800

    Trying to fix database config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,14 +9,11 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     if let databaseURL = Environment.get("DATABASE_URL") {
-        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        var postgresConfig = PostgresConfiguration(url: databaseURL)
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
         tlsConfig.certificateVerification = .none
-        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
-        
-        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
-        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
-        
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+        postgresConfig?.tlsConfiguration = tlsConfig
+        app.databases.use(.postgres(configuration: postgresConfig!), as: .psql)
     }  else {
         app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
